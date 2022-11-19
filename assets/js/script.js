34af64dff196d6328d5ef51c8256618ebfe57bb3
Fernando <fernandotrujillo@Fernandos-MBP.attlocal.net>
2022-11-19 15:31:38 -0500

    starter code

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var key = '07c8d1e85bf89680f48a4f02d1c2e6e4';
+var city = 'Asheville';
+console.log(city);
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
